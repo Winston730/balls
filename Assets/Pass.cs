acb12e2e88df3adf746e210230ec6e39f96dd718
Pavel <pashaming14@gmail.com>
2022-10-23 00:29:44 +0300

    123

--- Assets/Pass.cs
+++ Assets/Pass.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Pass : MonoBehaviour
 {
-    public GameObject rus = GameObject.Find("RUSSIA");
-    public GameObject usa;
-    public GameObject uk;
-   void OnCollisionEnter2D(Collision2D other)
-   {
-        if(other.gameObject == rus) Destroy(rus);
-   }
+    public GameObject pass; 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(pass != other.gameObject)
+        {
+            Destroy(other.gameObject);
+        }
+       
+    }
 }
