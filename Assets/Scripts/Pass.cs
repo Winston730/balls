e796ff4b37b2b23ffaf45b06eb5548badd879452
Pavel <pashaming14@gmail.com>
2022-10-23 04:45:21 +0300

    big update

--- Assets/Scripts/Pass.cs
+++ Assets/Scripts/Pass.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pass : MonoBehaviour
 {
-     void OnCollisionEnter2D(Collision2D other)
+    public int amountOfCountry;
+    private int counter = 0;
+
+    void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(other.gameObject);
+        counter++;
+        if(counter <= amountOfCountry - 1) 
+        {   
+            if(counter == amountOfCountry - 1) 
+            {
+                gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+            }
+            Destroy(other.gameObject);
+        } 
     }
 }
